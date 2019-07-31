2213d68f4bd872a4266d1b8fccd9276009f312eb
Vitaly Mes <volkondr@ya.ru>
2019-07-31 14:37:34 +0300

    Исправляет ошибки в карусели

--- source/js/carousel.js
+++ source/js/carousel.js
@@ -42,3 +42,29 @@ $('.popup__gallery').magnificPopup({
 			}
 		}
 	});
+
+  $('.popup__gallery-2').magnificPopup({
+  		delegate: 'a',
+  		type: 'image',
+  		removalDelay: 200, //delay removal by X to allow out-animation
+  		callbacks: {
+  			beforeOpen: function() {
+  				// just a hack that adds mfp-anim class to markup
+  				 this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+  				 this.st.mainClass = this.st.el.attr('data-effect');
+  			}
+  		},
+  		tLoading: 'Loading image #%curr%...',
+  		mainClass: 'mfp-img-mobile',
+  		gallery: {
+  			enabled: true,
+  			navigateByImgClick: true,
+  			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+  		},
+  		image: {
+  			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+  			titleSrc: function(item) {
+  				return item.el.attr('title') + '<small></small>';
+  			}
+  		}
+  	});
